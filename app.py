d8ef36582427aae3fe0236deef107d2c7f4b0e36
Nitin <nitin@Admins-MacBook-Air-6.local>
2025-03-23 16:51:39 +0530

    Initial commit: Flask project setup

---
+++ app.py
@@ -0,0 +1,37 @@
+# imports
+from flask import Flask, render_template
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+# My App Setup
+app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+# Data class -> Row of data
+class Mytask(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    content = db.Column(db.String(100) , nullable = False)
+    complete = db.Column(db.Integer, default= 0)
+    created = db.Column(db.DateTime , default= datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+
+
+
+# Routes for webpages
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Runner and Debugger
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
